ffbb7e8f064211c6691b9eaf6f7f457db539f7f5
asma <asmatra149@gmail.com>
2023-05-27 12:15:36 +0100

    first commit

---
+++ TunisCamp/src/main/java/com/example/tuniscamp/TunisCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.tuniscamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunisCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TunisCampApplication.class, args);
+    }
+
+}
