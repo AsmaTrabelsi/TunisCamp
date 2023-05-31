beb9cf07824fb12a715a85308b9765bdd819e74c
asma <asmatra149@gmail.com>
2023-05-31 21:30:41 +0100

    fixing bug

--- TunisCamp/src/main/java/com/example/tuniscamp/repositories/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tuniscamp.repositories;
-
-import com.example.tuniscamp.entities.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event,Integer> {
-}
