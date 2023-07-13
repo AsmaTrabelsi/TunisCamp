4c2cf07b948dd0da57413f906fba571982c01faf
WissalTa98 <133794472+wissalEy@users.noreply.github.com>
2023-07-13 13:53:18 +0100

    Adding Interceptor and Guard

--- clientSide/src/app/Components/event/event.module.ts
+++ clientSide/src/app/Components/event/event.module.ts
@@ -8,6 +8,7 @@ import { GoogleMapComponent } from './google-map/google-map.component';
 import { EventListComponent } from './event-list/event-list.component';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatSliderModule} from '@angular/material/slider';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
 import { EventCardComponent } from './event-card/event-card.component';
 import { AddEventComponent } from './add-event/add-event.component';
 import { FormsModule } from '@angular/forms';
@@ -48,6 +49,7 @@ import { RouterModule } from '@angular/router';
         GoogleMapsModule,
         MatChipsModule,
         MatSliderModule,
+        NgxSliderModule,
         NgxDropzoneModule,
         MatExpansionModule,
         MatButtonModule,
